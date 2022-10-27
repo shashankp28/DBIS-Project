eb29c454e7ab98dc71c14cf2b2e9bd9ad1aaaf11
Varendra Maurya <varendramaurya@gmail.com>
2022-10-27 17:55:15 +0530

    updated database schema

--- init.sql
+++ init.sql
@@ -4,61 +4,70 @@ create table person(
     first_name varchar(30) not null,
     middle_name varchar(30),
     last_name varchar(30),
-    dob DATE check(DATEDIFF(year, dob, getdate())>18) not null ,
+    dob DATE not null ,
     address_first_line varchar(50) not null,
     address_second_line varchar(50),
-    zip_code int not null check(zip),
+    zip_code int not null check(zip>0),
     country varchar(25) not null,
     gender ENUM('male', 'female', 'others'),
     path_to_resume varchar(100) not null,
-    primary key (email_id)
+    primary key (email_id),
+    foreign key email_id references credentials(email_id)
 );
 
 create table employee(
     email_id varchar(50) not null,
     salary int not null check(salary>0),
-    position varchar(30) not null,
-    role_id varchar(10) not null,
+    -- position varchar(30) not null,
+    -- role_id varchar(10) not null,
     primary key email_id,
     foreign key email_id references person,
-    foreign key role_id references role(id)
+    -- foreign key role_id references role(id)
 );
 
-create table stage(
-    stage_id int not null,
-    description varchar(50) not null,
-    primary key stage_id
-);
-
-create table applicant(
+create table has_role(
     email_id varchar(50) not null,
-    interviewer_id varchar(50),
-    schedule date,
-    stage_id int not null,
-    primary key email_id,
-    foreign key email_id references person,
-    foreign key interviewer_id references employee(email_id),
-    foreign key stage_id references stage
-);
+    role_id varchar(10) not null,
+    primary key (email_id, role_id),
+    foreign key email_id references person(email_id),
+    foreign key role_id role(id)  
+)
+
+-- create table stage(
+--     stage_id int not null,
+--     description varchar(50) not null,
+--     primary key stage_id
+-- );
+
+-- create table applicant(
+--     email_id varchar(50) not null,
+--     interviewer_id varchar(50),
+--     schedule date,
+--     stage_id int not null,
+--     primary key email_id,
+--     foreign key email_id references person,
+--     foreign key interviewer_id references employee(email_id),
+--     foreign key stage_id references stage
+-- );
 
 create table intern(
     email_id varchar(50) not null,
     stipend int check(stipend>0),
     mentor_id varchar(50) not null,
-    position varchar(30) not null,
-    role_id varchar(10) not null,
+    -- position varchar(30) not null,
+    -- role_id varchar(10) not null,
     start_date DATE not null,
     expected_end_date DATE not null,
     -- current_status BOOLEAN not null,
     primary key email_id,
     foreign key mentor_id references employee(email_id),
     foreign key email_id references person,
-    foreign key role_id references role(id)
+    -- foreign key role_id references role(id)
 );
 
 create table is_completed(
     email_id varchar(50) not null,
-    actual_end_date DATE not null,
+    end_date DATE not null,
     score int not null check(score>=0 and score<=10),
     performance_desc varchar(1000),
     primary key email_id,
@@ -76,14 +85,78 @@ create table role(
     primary key id
 );
 
+create table location(
+    location_id varchar(10) not null,
+    address varchar(50) not null,
+    city varchar(30) not null,
+    country varchar(30) not null,
+    primary key location_id
+);
+
+create table at_location(
+    email_id varchar(50) not null,
+    location_id varchar(10) not null unique,
+    primary key email_id,
+    foreign key email_id references person(email_id),
+    foreign key location_id references location(location_id)
+)
+
+create table project(
+    project_id varchar(10) not null,
+    topic varchar(100) not null,
+    description varchar(1000) not null,
+    path_to_pdf varchar(200) not null,
+    primary key project_id
+);
+
+create table assigned_to(
+    email_id varchar(50) not null,
+    project_id varchar(10) not null,
+    assigned_date date not null,
+    end_date date,
+    primary key email_id,
+    foreign key email_id references intern(email_id),
+    foreign key project_id references project(project_id)
+)
+
+create table university(
+    university_id varchar(10) not null,
+    name varchar(100) not null,s
+    rank int check(rank>=0),
+    city varchar(30) not null,
+    country varchar(30) not null,
+    primary key university_id
+);
+
+create table studies_in(
+    email_id varchar(50) not null,
+    university_id varchar(10) not null,
+    cpi decimal(10,2) not null,
+    passing_year int not null check(passing_year>2000),
+    primary key email_id,
+    foreign key email_id references intern(email_id),
+    foreign key university_id references university(university_id)
+);
+
+
+
+create table credentials(
+    email_id varchar(50) not null,
+    password varchar(100) not null,
+    primary key email_id
+);
+
 -- create table previous_interns(
 --     email_id varchar(50) not null,
 --     stipend int check(stipend>0),
 --     mentor_id varchar(50) not null,
---     position varchar(30) not null,
---     role_id varchar(10) not null,
+--     end_date date not null,
+--     start_date date not null,
+--     score int check(score>=0 and score<=10) not null,
+--     performance_desc 
+--     -- role_id varchar(10) not null,
 --     primary key email_id,
 --     foreign key mentor_id references employee(email_id),
 --     foreign key email_id references person,
---     foreign key role_id references role(id)
+--     -- foreign key role_id references role(id)
 -- );
